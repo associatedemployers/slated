80feeb1570836d4b76bdd63b61aeb61598d470f7
James-Mac <james@aehr.org>
2016-07-09 23:59:15 -0600

    Update with target servers

---
+++ target-core.js
@@ -0,0 +1,27 @@
+process.title = 'Slated Target - Node.js';
+
+var express    = require('express'),
+    winston    = require('./lib/winston')(),
+    chalk      = require('chalk'),
+    bodyParser = require('body-parser'),
+    morgan     = require('morgan'),
+    app        = express();
+
+var handler = require('./lib/target'),
+    port    = process.env.PORT || 4544;
+
+winston.debug(chalk.dim('Setting server options...'));
+app.disable('x-powered-by');
+winston.debug(chalk.dim('Setting up middleware...'));
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.post('/deploy', handler.deploy);
+
+app.init().listen(port, function () {
+  winston.info(chalk.dim('Slated listening on port:', port));
+});
